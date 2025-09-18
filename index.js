4c87d533f2ef270f828a533dce336cd2cbeeb2ff
suvitcha <boom.suvitcha@gmail.com>
2025-09-18 10:30:06 +0700

    add render2

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://frontend-gadme-vercel.vercel.app"
+    "https://frontend-gadme-vercel-order.vercel.app",
   ], // frontend domain
   credentials: true,
 };
